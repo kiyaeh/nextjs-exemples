5aaecf9f925d53e07c90f5015ee22a403389fa92
Ewnetu Tesfaye <151930026+kiyaeh@users.noreply.github.com>
2025-02-08 23:58:57 +0300

    typescript

---
+++ my-ts-next-app/src/types/page.tsx
@@ -0,0 +1,28 @@
+// let kiya="kiyaeh";
+// console.log(kiya)
+// // kiya=12;
+// const typesScripcFunc=():String=>{
+//  return "kiya";
+// //  console.log(3 + 5);
+// }
+// typesScripcFunc();
+// function add(a: number, b: number): number {
+//     return a + b;
+//   }
+//   add(3,8);
+// //   add("kiya",4)
+
+// Objects and Arrays
+interface User{
+    name:string,
+    age:number,
+    email:string
+}
+const user:User={
+    name:"kiya",
+    age:23,
+    email:"kiyaeh"
+}
+const arrayExampple:String[]=['kiya','nruo']
+arrayExampple.push("kiyaw")
+console.log(arrayExampple);
